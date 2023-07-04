1828b852994d0fdd043d0a41b385a1655f9ecdc2
Knight <1621828432@qq.com>
2023-07-04 20:24:35 +0800

    推送

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,12 +2,6 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git4!");
-        System.out.println("hello git4!");
-        System.out.println("hello git4!");
-        System.out.println("hello git4!");
         System.out.println("hello git4!");
         System.out.println("hello git4!");
     }
